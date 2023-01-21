be26c8a07ed5f3a962533a2405e1918d845273c5
pyongjie <pyjavatar@gmail.com>
2023-01-21 22:33:55 +0800

    Add greeting comment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 public class Main {
 
+    // hope you enjoyed!
+    
     public static void main(String[] args) {
         LoanManager m1 = new LoanManager();
         m1.addLoan("Alice Betsy", 10, 0.1);
